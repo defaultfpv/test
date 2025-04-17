d8be2b6672a00cc3d8e2c3b7e9caa2ed27b5691e
default <babchenkod1999@gmail.com>
2025-04-17 14:17:33 +0300

    Исправил баг с длиной столбца message

--- database/migrations/2025_04_11_141913_create_messages_table.php
+++ database/migrations/2025_04_11_141913_create_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('author');
-            $table->string('message');
+            $table->text('message');
             $table->timestamps();
         });
     }
